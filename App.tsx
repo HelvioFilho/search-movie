7cc99f22a97b33bfbae708387dc0572fc3686b4a
Hélvio Filho <helviosvf@gmail.com>
2021-12-21 22:35:49 -0300

    creating screens

--- App.tsx
+++ App.tsx
@@ -3,7 +3,7 @@ import { ThemeProvider } from 'styled-components';
 
 import { defaultTheme } from './src/global/styles/theme';
 
-import { StyleSheet, Text, View } from 'react-native';
+import { Text } from 'react-native';
 
 
 export default function App() {
---
+++ src/screens/Movies/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Container, Title } from './styles';
+
+export function Movies() {
+  return (
+    <Container>
+      <Title> Tela Movies</Title>
+    </Container>
+  );
+}
