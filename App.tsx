89c4f66b799f038ea2b478042fc5a2a6d19f5d07
Hélvio Filho <helviosvf@gmail.com>
2021-12-22 02:07:03 -0300

    Navigation Home and Movies

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
 import 'react-native-gesture-handler';
 import React from 'react';
+import { StatusBar } from 'react-native';
 import { ThemeProvider } from 'styled-components';
 
 import { defaultTheme } from './src/global/styles/theme';
@@ -10,6 +11,7 @@ import { Routes } from './src/routes';
 export default function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
+      <StatusBar hidden={true} />
       <Routes />
     </ThemeProvider>
   );
--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,16 +1,71 @@
 import React from "react";
 import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { ParamListBase } from '@react-navigation/native';
-
+import { defaultTheme } from '../global/styles/theme';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { Home, Movies } from '../screens';
+
+function StackRoute() {
+  const { Navigator, Screen } = createNativeStackNavigator<ParamListBase>();
+
+  return (
+    <Navigator>
+      <Screen
+        name="Home"
+        component={Home}
+        options={{
+          headerShown: false,
+        }}
+      />
+    </Navigator>
+  )
+}
 
 export function AppRoutes() {
   const { Navigator, Screen } = createDrawerNavigator<ParamListBase>();
 
   return (
-    <Navigator>
-      <Screen name="Home" component={Home} />
-      <Screen name="Movies" component={Movies} />
+    <Navigator
+      screenOptions={{
+        headerShown: false,
+        drawerStyle: {
+          backgroundColor: defaultTheme.colors.black,
+          paddingTop: defaultTheme.metrics.px(20),
+        },
+        drawerActiveBackgroundColor: defaultTheme.colors.cta,
+        drawerActiveTintColor: defaultTheme.colors.white,
+        drawerInactiveTintColor: defaultTheme.colors.white,
+      }}
+    >
+      <Screen
+        name="HomeDrawer"
+        component={StackRoute}
+        options={{
+          title: 'Home',
+          drawerIcon: ({ focused, size, color }) => (
+            <MaterialCommunityIcons
+              name={focused ? "movie-open" : "movie-outline"}
+              size={size}
+              color={color}
+            />
+          )
+        }}
+      />
+      <Screen
+        name="Movies"
+        component={Movies}
+        options={{
+          title: 'Meus Filmes',
+          drawerIcon: ({ focused, size, color }) => (
+            <MaterialCommunityIcons
+              name={focused ? "archive" : "archive-outline"}
+              size={size}
+              color={color}
+            />
+          )
+        }}
+      />
     </Navigator>
   );
 }
