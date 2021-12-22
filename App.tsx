1cc00a82e0d1193421a663b3f5eb28c7425dd676
Hélvio Filho <helviosvf@gmail.com>
2021-12-21 23:31:02 -0300

    fix animated

--- App.tsx
+++ App.tsx
@@ -1,15 +1,16 @@
+import 'react-native-gesture-handler';
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
 
 import { defaultTheme } from './src/global/styles/theme';
 
-import { Text } from 'react-native';
+import { Routes } from './src/routes';
 
 
 export default function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Text>Meu App</Text>
+      <Routes />
     </ThemeProvider>
   );
 }
--- src/screens/Movies/styles.ts
+++ src/screens/Movies/styles.ts
@@ -8,5 +8,5 @@ export const Container = styled.View`
 `;
 
 export const Title = styled.Text`
-  size: 15px;
+  
 `;
