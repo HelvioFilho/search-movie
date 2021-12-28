2c1f55580fe29c357c5401e9be713aac53616f65
Hélvio Filho <helviosvf@gmail.com>
2021-12-28 18:25:47 -0300

    IconButton updated

--- src/components/atoms/IconButton/index.tsx
+++ src/components/atoms/IconButton/index.tsx
@@ -1,22 +1,30 @@
 import React from 'react';
-
-import { Feather } from '@expo/vector-icons';
+import { Feather, Ionicons } from '@expo/vector-icons';
 import { Icon } from './styles';
 import { defaultTheme } from '../../../global';
 import { IconProps } from '../../../utils/interface';
 
-export function IconButton({ name, size, color, onPress, ...props }: IconProps) {
+export function IconButton({ feather, ionicons, size, color, onPress, ...props }: IconProps) {
 
   return (
     <Icon
       onPress={onPress}
       {...props}
     >
-      <Feather
-        name={name}
-        size={size}
-        color={defaultTheme.colors[color || 'white']}
-      />
+      {feather &&
+        <Feather
+          name={feather}
+          size={size}
+          color={defaultTheme.colors[color || 'white']}
+        />
+      }
+      {ionicons &&
+        <Ionicons
+          name={ionicons}
+          size={size}
+          color={defaultTheme.colors[color || 'white']}
+        />
+      }
     </Icon>
   );
 }
